2f8a2c6d181f3faedf732da1014b602559001e96
Diyah <me@hadiyah.me>
2013-06-02 09:47:35 -0700

    add filepicker.io add item image url

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+if Rails.env.development?
+  heroku_env = File.join('config', 'heroku_env.rb')
+  load(heroku_env) if File.exists?(heroku_env)
+end
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+
 
 if Rails.env.production?
   Pony.options = {
