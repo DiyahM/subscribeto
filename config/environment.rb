8d944abc08478aba0ae3d4db71403736d5160cb1
Diyah <me@hadiyah.me>
2013-07-11 04:35:10 -0700

    add mail settings for password reset

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,18 @@ if Rails.env.production?
       :enable_starttls_auto => true
     }
   }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
+
+
+
 # Initialize the rails application
 Subscribeto::Application.initialize!
